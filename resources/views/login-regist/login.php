eb8ea6430530707c173f862278341e5472f3ee99
MrShoza <yaztex771@gmail.com>
2024-11-13 06:27:38 +0900

    update yeh

---
+++ resources/views/login-regist/login.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/login.css">
+
+</head>
+<body>
+    <div class="goblok">
+        <form>
+            <div class="anjay">
+                <a href="../"><i class="fa fa-arrow-left"> Kembali</i></a>
+            </div>
+            <h1>Login</h1>
+            <div class="uwu">
+                <i class="fa fa-user"> User</i>
+                   <input type="text" placeholder="Masukan Username" required>
+                        <i class="fa fa-lock"> Password</i>
+                        <input type="password" placeholder="Masukan Password" required>
+                   <input type="submit" value="Login">
+                <small>Belum punya account? <a href="register.html">Register</a></small>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
