e9215605fb823ccb6ebfb82751818a3a2c72d302
MrShoza <yaztex771@gmail.com>
2024-11-13 15:13:31 +0900

    LOLz

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('judul')->unique();
             $table->string('foto');
             $table->string('deskripsi');
+            $table->timestamps();
         });
     }
 
